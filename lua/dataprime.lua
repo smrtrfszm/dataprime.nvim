2b45fe269181041277fd93dd0c6c8c1bbf54ec74
Szepesi Tibor <smrtrfszm@proton.me>
2025-05-30 20:55:54 +0200

    set commentstring in ftplugin

--- lua/dataprime.lua
+++ lua/dataprime.lua
@@ -10,13 +10,6 @@ function M.setup()
 
   vim.treesitter.language.register('dataprime', 'dataprime')
 
-  vim.api.nvim_create_autocmd('FileType', {
-    pattern = 'dataprime',
-    callback = function ()
-      vim.bo.commentstring = '//%s'
-    end,
-  })
-
   local ok, ft = pcall(require, 'Comment.ft')
   if ok then
     ft.set('dataprime', {'//%s', '/*%s*/'})
